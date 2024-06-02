d4e33154bf5bdcf4b253b3bf39b3d365d945e2d7
amrasawa <karmelsalma5@gmail.com>
2024-06-02 21:26:52 +0300

    first js

---
+++ style/main.js
@@ -0,0 +1,3 @@
+document.querySelector(".userProfile h2").textContent="Hello, I'm Amra";
+document.querySelector(".userInfo p").classList.add("amra");
+console.log(document.querySelector(".userProfile h2"));
